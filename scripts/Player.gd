ce214bcd2bdc674331cb0e9ca0dbd0b33dfe6505
_michdev <150397626+mich-gamedev@users.noreply.github.com>
2024-02-29 19:16:41 -0500

    updated player anims

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var right_check: Area2D = $Placer/RightCheck
 @onready var up_check: Area2D = $Placer/UpCheck
 @onready var down_check: Area2D = $Placer/DownCheck
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const cell_size = 32
 
@@ -24,21 +25,27 @@ func tween_new_position():
 
 func handle_inputs() -> void:
 	if Input.is_action_just_pressed("move_left") and !left_check.has_overlapping_bodies():
+		anim.play(&"dash")
+		anim.flip_h = true
 		placer.position += Vector2.LEFT * cell_size
 		PlayerStats.can_teleport = true
 		tween_new_position()
 
 	if Input.is_action_just_pressed("move_right") and !right_check.has_overlapping_bodies():
+		anim.play(&"dash")
+		anim.flip_h = false
 		placer.position += Vector2.RIGHT * cell_size
 		PlayerStats.can_teleport = true
 		tween_new_position()
 
 	if Input.is_action_just_pressed("move_up") and !up_check.has_overlapping_bodies():
+		anim.play(&"dash")
 		placer.position += Vector2.UP * cell_size
 		PlayerStats.can_teleport = true
 		tween_new_position()
 
 	if Input.is_action_just_pressed("move_down") and !down_check.has_overlapping_bodies():
+		anim.play(&"dash")
 		placer.position += Vector2.DOWN * cell_size
 		PlayerStats.can_teleport = true
 		tween_new_position()
